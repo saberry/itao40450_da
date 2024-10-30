8a142436715e3855b13a405bc55cfdf8de9a28cf
Seth Berry <seth.berry@nd.edu>
2024-10-30 10:08:04 -0400

    Adding scorecard link

--- api_parse_example.R
+++ api_parse_example.R
@@ -14,12 +14,3 @@ fastest_sacks <- request(link) %>%
 
 
 # https://collegescorecard.ed.gov/data/api-documentation/
-#  alOlYg5uXqZR312NFftsu5UUYldGDox03bcveNpR 
-
-link <- "https://api.data.gov/ed/collegescorecard/v1/schools?api_key=alOlYg5uXqZR312NFftsu5UUYldGDox03bcveNpR&school.name=University%20of%20Notre%20Dame&page=0per_page=50"
-
-score_card <- request(link) %>% 
-  req_perform() %>% 
-  resp_body_json()
-
-score_card$results[[1]]$latest$student$demographics
